b55b2692b76ca4f6fa1f68ae60d0473f6f8b15d3
jiashi-feng <shij5412@gmail.com>
2025-03-15 15:28:25 +0800

    four

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/Hello';
+import App from './src/App';
 import {name as appName} from './app.json';
-  console.log('Hello World!');
-
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/Index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import styles from './styles'
+export default class Index extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={[{color:'red'},{fontSize:25},{fontWeight:'bold'},{margin:81}]}> 家务任务自动分配软件 </Text>
+        <Text style={styles.instructions}> 核心需求： </Text>
+        <Text style={styles.welcome}> 任务分配及公平性 </Text>
+        <Text style={styles.welcome}> 任务跟踪与提醒 </Text>
+        <Text style={styles.welcome}> 任务分类与优先级 </Text>
+        <Text style={styles.welcome}> 多设备同步 </Text>
+        <Text style={styles.welcome}> 奖励机制 </Text>
+        <Text style={styles.welcome}> 隐私保护与数据安全 </Text>
+        <Text style={styles.instructions}> 用户角色： </Text>
+        <Text style={styles.welcome}> 管理员（家长） </Text>
+        <Text style={styles.welcome}> 成员（孩子、配偶） </Text>
+        
+      </View>
+    )
+  }
+}
+
